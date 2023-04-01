3da3e6b2d5036823c83a7698381e0e3f58a01e45
홍혁준 <jjj8337721@naver.com>
2023-04-01 11:28:14 +0900

    feat : 연관 테이블로 다대다 연관관계를 매핑하기 위해 연관 테이블 생성 - VetSpecialty Entity - 단방향으로 연관 - Cascade   - MERGE : 연관 엔티티와 변경사항 병합   - PERSIST : 연관 엔티티와 함께 새로운 엔티티 저장

--- src/main/java/kr/co/hhjpetclinicstudy/persistence/entity/Vet.java
+++ src/main/java/kr/co/hhjpetclinicstudy/persistence/entity/Vet.java
@@ -7,6 +7,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tbl_vet")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -23,10 +26,20 @@ public class Vet extends BaseEntity {
     @Column(name = "last_name", length = 30)
     private String lastName;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+        name = "vet_specialty",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id")
+    )
+    private List<Specialty> specialtyList = new ArrayList<>();
+
     @Builder
     public Vet(String firstName,
-               String lastName) {
+               String lastName,
+               List<Specialty> specialtyList) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.specialtyList = specialtyList;
     }
 }
