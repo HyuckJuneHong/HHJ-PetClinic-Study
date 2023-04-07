0e7c3bad62c7c3a66af78f8eb6558fe7948516e8
홍혁준 <jjj8337721@naver.com>
2023-04-08 01:10:07 +0900

    feat : Vet Exception 처리

--- src/main/java/kr/co/hhjpetclinicstudy/controller/VetController.java
+++ src/main/java/kr/co/hhjpetclinicstudy/controller/VetController.java
@@ -1,10 +1,14 @@
 package kr.co.hhjpetclinicstudy.controller;
 
 import jakarta.validation.Valid;
+import kr.co.hhjpetclinicstudy.infrastructure.error.exception.NotFountException;
+import kr.co.hhjpetclinicstudy.infrastructure.error.model.ResponseFormat;
+import kr.co.hhjpetclinicstudy.infrastructure.error.model.ResponseStatus;
 import kr.co.hhjpetclinicstudy.service.service.VetService;
 import kr.co.hhjpetclinicstudy.service.model.dtos.request.VetReqDTO;
 import kr.co.hhjpetclinicstudy.service.model.dtos.response.VetResDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,45 +20,45 @@ public class VetController {
     private final VetService vetService;
 
     @PostMapping
-    public ResponseEntity<String> createVet(@RequestBody @Valid VetReqDTO.CREATE create){
+    public ResponseFormat<Void> createVet(@RequestBody @Valid VetReqDTO.CREATE create){
 
         try {
             vetService.createVet(create);
-            return ResponseEntity.ok().body("Success Vet Create");
-        } catch (Exception e){
-            return ResponseEntity.ok().body("error : " + e);
+            return ResponseFormat.success(ResponseStatus.SUCCESS_CREATE);
+        } catch (RuntimeException e){
+            return ResponseFormat.error(ResponseStatus.FAIL_BAD_REQUEST);
         }
     }
 
     @GetMapping("/{vet_id}")
-    public ResponseEntity<VetResDTO.READ> getVetById(@PathVariable(name = "vet_id") Long vetId) throws Exception {
+    public ResponseFormat<VetResDTO.READ> getVetById(@PathVariable(name = "vet_id") Long vetId) {
 
         try {
-            return ResponseEntity.ok().body(vetService.getVetById(vetId));
-        } catch (Exception e){
-            throw new Exception("error : " + e);
+            return ResponseFormat.successWithData(ResponseStatus.SUCCESS_OK, vetService.getVetById(vetId));
+        } catch (NotFountException e){
+            return ResponseFormat.error(ResponseStatus.FAIL_NOT_FOUND);
         }
     }
 
     @PutMapping
-    public ResponseEntity<String> updateVet(@RequestBody VetReqDTO.UPDATE update){
+    public ResponseFormat<Void> updateVet(@RequestBody VetReqDTO.UPDATE update){
 
         try {
             vetService.updateVet(update);
-            return ResponseEntity.ok().body("Success Vet Update");
-        } catch (Exception e){
-            return ResponseEntity.ok().body("error : "+ e);
+            return ResponseFormat.success(ResponseStatus.SUCCESS_NO_CONTENT);
+        } catch (NotFountException e){
+            return ResponseFormat.error(ResponseStatus.FAIL_NOT_FOUND);
         }
     }
 
     @DeleteMapping("/{vet_id}")
-    public ResponseEntity<String> deleteVetById(@PathVariable(name = "vet_id") Long vetId){
+    public ResponseFormat<Void> deleteVetById(@PathVariable(name = "vet_id") Long vetId){
 
         try {
             vetService.deleteVetById(vetId);
-            return ResponseEntity.ok().body("Success Delete Vet");
-        } catch (Exception e){
-            return ResponseEntity.ok().body("error : " + e);
+            return ResponseFormat.success(ResponseStatus.SUCCESS_NO_CONTENT);
+        } catch (NotFountException e){
+            return ResponseFormat.error(ResponseStatus.FAIL_NOT_FOUND);
         }
     }
 }
