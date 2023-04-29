d33c2a15e7a4ee409e64f1c73d8a505d3d7c855e
홍혁준 <jjj8337721@naver.com>
2023-04-29 21:50:15 +0900

    refactor : Log Execution Time Logging Aspect Issue

--- src/main/java/kr/co/hhjpetclinicstudy/infrastructure/logging/ExecutionTimeLoggingAspect.java
+++ src/main/java/kr/co/hhjpetclinicstudy/infrastructure/logging/ExecutionTimeLoggingAspect.java
@@ -14,20 +14,24 @@ import org.springframework.util.StopWatch;
 public class ExecutionTimeLoggingAspect {
 
     @Pointcut("execution(* kr.co.hhjpetclinicstudy.service.service.OwnerService.*(..))")
-    private void allOwnerService(){ }
+    private void allOwnerService() {
+    }
 
     @Pointcut("execution(* kr.co.hhjpetclinicstudy.service.service.PetService.*(..))")
-    private void allPetService(){ }
+    private void allPetService() {
+    }
 
     @Pointcut("execution(* kr.co.hhjpetclinicstudy.service.service.VetService.*(..))")
-    private void allVetService(){ }
+    private void allVetService() {
+    }
 
     @Pointcut("execution(* kr.co.hhjpetclinicstudy.service.service.VisitService.*(..))")
-    private void allVisitService(){ }
+    private void allVisitService() {
+    }
 
     //TODO : @issue - 현재 메소드 내부에서 다른 메소드를 호출하는 상황에서 내부 메도스에 해당 AOP가 적용되지 않는 이슈 발생.
     @Around("allOwnerService() || allPetService() || allVetService() || allVisitService()")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable{
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
         String className = joinPoint.getTarget().getClass().getSimpleName();
 
