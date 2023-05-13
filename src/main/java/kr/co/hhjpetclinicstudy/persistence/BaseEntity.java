ab897d5d0664b7caa8f1edd208da4c80129f64af
HyuckJuneHong <jjj8337721@naver.com>
2023-05-14 00:17:27 +0900

    feat : Auditing

--- src/main/java/kr/co/hhjpetclinicstudy/persistence/BaseEntity.java
+++ src/main/java/kr/co/hhjpetclinicstudy/persistence/BaseEntity.java
@@ -3,13 +3,27 @@ package kr.co.hhjpetclinicstudy.persistence;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @MappedSuperclass
 @EqualsAndHashCode(of = "id", callSuper = false)
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    protected LocalDate modifiedAt;
 }
