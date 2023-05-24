13b858abfe064de545ec4bc101da1e0e09ff1f58
HyuckJuneHong <jjj8337721@naver.com>
2023-05-24 16:37:43 +0900

    feat : 인가 처리 제어

--- src/main/java/kr/co/hhjpetclinicstudy/infrastructure/config/SecurityConfig.java
+++ src/main/java/kr/co/hhjpetclinicstudy/infrastructure/config/SecurityConfig.java
@@ -1,24 +1,33 @@
 package kr.co.hhjpetclinicstudy.infrastructure.config;
 
 import jakarta.servlet.http.HttpSession;
+import kr.co.hhjpetclinicstudy.service.model.enums.UserRole;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
 
         //인가 정책
         http
+                .securityMatcher("/admin/**")
                 .authorizeHttpRequests()
+                    .requestMatchers("/admin/login").permitAll()
+                    .requestMatchers("/admin/**").hasRole(UserRole.ADMIN_ROLE.getUserRole())
                 .anyRequest().authenticated();
 
         //인증 정책
@@ -28,21 +37,11 @@ public class SecurityConfig {
         //로그아웃
         http
                 .logout()
-                .logoutUrl("/logout")
                 .logoutSuccessUrl("/login")
-                .addLogoutHandler(logoutHandler())
                 .logoutSuccessHandler(logoutSuccessHandler())
                 .deleteCookies("remember-me");
 
         return http.build();
-    }
-
-    private LogoutHandler logoutHandler() {
-
-        return (request, response, authentication) -> {
-            HttpSession session = request.getSession();
-            session.invalidate();
-        };
     }
 
     private LogoutSuccessHandler logoutSuccessHandler() {
